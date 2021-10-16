f213744183815dbe3a7003417a231f3f471ca51e
Steven Essam <stevenessams7@gmail.com>
2021-10-16 11:20:06 +0200

    Page Chantier V1.1 fichier bootstrapstyle

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('supernova', './assets/styles/supernova.css')
+    .addEntry('bootstrapStyle', './assets/styles/bootstrapStyle.css')
     .addEntry('feature', './assets/styles/feature.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
