97490beae4e7ea627afc6aa140ba1773b07ebbd3
Moha-Dan <73938112+Moha-Dan@users.noreply.github.com>
2021-11-25 20:21:25 +0100

    input+dialog

--- assets/haiku.js
+++ assets/haiku.js
@@ -456,7 +456,61 @@ function UpdateFilter(){
 		
 	})
 }
+function UpdateInputs(){
+	// checkbox
+	const checkboxes = document.querySelectorAll('input[type=checkbox]')
+	checkboxes.forEach(checkbox=>{
+		checkbox.style.display = "none"
+		const close = ()=>{
+			ctx.strokeStyle = "#f00"
+			ctx.stroke(new Path2D("M0,0L100,100M0,100L100,0"))
+		}
+		var ctx = document.createElement("canvas").getContext('2d')
+		ctx.canvas.width = ctx.canvas.height = 100
+		ctx.lineWidth = 20
+		ctx.canvas.checked = false
+		const update = ()=>{
+			ctx.clearRect(0,0,100,100)
+			if(ctx.canvas.checked = checkbox.checked){
+				ctx.strokeStyle = "#0c2"
+				ctx.stroke(new Path2D("M0,25L30,75L100,25"))
+			}else{
+				close()
+			}
+			ctx.canvas.dispatchEvent(new Event("change",{}))
+		}
+		ctx.canvas.addEventListener('click',()=>{checkbox.checked = !checkbox.checked;update()})
+		ctx.canvas.onchange = checkbox.onchange
+		checkbox.onchange = null
+		checkbox.addEventListener('change',()=>{update()})		
+		close()
+		ctx.canvas.style = "width:2em;border:rgb(var(--color)) solid .2em;border-radius:1em;"
+		checkbox.after(ctx.canvas)
+	})
+}
+function UpdateDialog(){
+	var hash = location.hash
+	if(hash.length<1)return
+	hash = document.querySelector(hash)
+	if(hash && hash.tagName == "DIALOG"){
+		hash.setAttribute('open',"true")
+		hash.close = function(){
+			this.removeAttribute('open')
+			document.body.classList.remove("dialog-focus")
+			location.hash = ""
+		}
+		document.body.classList.add("dialog-focus")
+	}
+}
+function $(selector){
+	return document.querySelector(selector)
+}
 window.addEventListener('load',()=>{
 	UpdateFilter()
 	UpdateTabber()
+	UpdateInputs()
+	UpdateDialog()
+})
+window.addEventListener('hashchange',()=>{
+	UpdateDialog()
 })
