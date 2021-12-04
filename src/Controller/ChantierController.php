df316cd56f88d26b5640a433cb8961cfe7071836
Alexandre Legal <503p2.alexandre.legal@gmail.com>
2021-12-04 14:39:54 +0100

    suppr template / route inutile

--- src/Controller/ChantierController.php
+++ src/Controller/ChantierController.php
@@ -30,27 +30,6 @@ class ChantierController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'chantier_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
-    {
-        $chantier = new Chantier();
-        $form = $this->createForm(ChantierType::class, $chantier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($chantier);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('chantier_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('chantier/new.html.twig', [
-            'chantier' => $chantier,
-            'form' => $form,
-        ]);
-    }
-
     /**
     *   @Route("/{id}",name="chantier_show")
     */
@@ -68,35 +47,5 @@ class ChantierController extends AbstractController
             'images'  => $images,
             'titre' => 'CHANTIER',
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'chantier_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Chantier $chantier): Response
-    {
-        $form = $this->createForm(ChantierType::class, $chantier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('chantier_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('chantier/edit.html.twig', [
-            'chantier' => $chantier,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'chantier_delete', methods: ['POST'])]
-    public function delete(Request $request, Chantier $chantier): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$chantier->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($chantier);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('chantier_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/DemandeDevisController.php
+++ src/Controller/DemandeDevisController.php
@@ -13,14 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/devis-contact')]
 class DemandeDevisController extends AbstractController
 {
-    #[Route('/t', name: 'demande_devis_new', methods: ['GET'])]
-    public function index(DemandeDevisRepository $demandeDevisRepository): Response
-    {
-        return $this->render('demande_devis/index.html.twig', [
-            'demande_devis' => $demandeDevisRepository->findAll(),
-        ]);
-    }
-
     #[Route('/', name: 'demande_devis_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
@@ -42,43 +34,5 @@ class DemandeDevisController extends AbstractController
             "titre" => "Demande de devis",
             'form' => $form,
         ]);
-    }
-
-    #[Route('/{id}', name: 'demande_devis_show', methods: ['GET'])]
-    public function show(DemandeDevis $demandeDevi): Response
-    {
-        return $this->render('demande_devis/show.html.twig', [
-            'demande_devi' => $demandeDevi,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'demande_devis_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, DemandeDevis $demandeDevi): Response
-    {
-        $form = $this->createForm(DemandeDevisType::class, $demandeDevi);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('demande_devis_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('demande_devis/edit.html.twig', [
-            'demande_devi' => $demandeDevi,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'demande_devis_delete', methods: ['POST'])]
-    public function delete(Request $request, DemandeDevis $demandeDevi): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$demandeDevi->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($demandeDevi);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('demande_devis_index', [], Response::HTTP_SEE_OTHER);
     }
 }
