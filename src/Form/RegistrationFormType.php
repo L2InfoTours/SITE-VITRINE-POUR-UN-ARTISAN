11080c1358859f230e9f7b72fc76f7ad49b8add3
Moha-Dan <73938112+Moha-Dan@users.noreply.github.com>
2021-12-05 17:20:47 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,11 +7,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
@@ -21,12 +23,15 @@ class RegistrationFormType extends AbstractType
             // ->add('email')
             // ->add('agreeTerms')
             // ->add('plainPassword')
-            ->add('email', EmailType::class,[
+            ->add('email', TextType::class,[
                 'required' => false,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Veuiller entrer une adresse mail',
-                    ])
+					]),
+					new Email([
+						'message' => 'The email "{{ value }}" is not a valid email.',
+					])
                     ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
